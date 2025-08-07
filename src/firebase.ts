c8d7d300003cc018be1fe00cdc883b9f3c4c87b1
Guica7 <guillerhaya7@gmail.com>
2025-08-07 15:05:45 +0200

    Actualizar configuración de Firebase a t4learningluca

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,12 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDuaf9g7tl3otHA8qFjlM7jgTFnx6qbPr0",
-  authDomain: "switchzeland-revolutionized.firebaseapp.com",
-  projectId: "switchzeland-revolutionized",
-  storageBucket: "switchzeland-revolutionized.appspot.com",
-  messagingSenderId: "751520990509",
-  appId: "1:751520990509:web:91368b091fb18748c2ffa4",
-  measurementId: "G-MBETG0P6SS"
+  apiKey: "AIzaSyC2ktQHVwr8TbV64_wFBbE_aob3ha0bNgE",
+  authDomain: "t4learningluca.firebaseapp.com",
+  projectId: "t4learningluca",
+  storageBucket: "t4learningluca.firebasestorage.app",
+  messagingSenderId: "614304141450",
+  appId: "1:614304141450:web:ad7963722850e79e08dd73"
 };
 
 const app = initializeApp(firebaseConfig);
